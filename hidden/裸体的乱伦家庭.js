bf90caedf59bc54cf84afc60f5fabea362e69054
INeed4000Bucks <119711646+INeed4000Bucks@users.noreply.github.com>
2022-12-30 16:38:07 -0500

    Updating Prelearning for mobile

--- hidden/裸体的乱伦家庭.js
+++ hidden/裸体的乱伦家庭.js
@@ -102,14 +102,21 @@ const lessonWords = [
 {word: "味", definition: "taste", score: 0}
     ];
 
-    const definition =  document.getElementById("definition")
-    const word = document.getElementById("word")
+    const definition = document.getElementById("definition");
+    const word = document.getElementById("word");
     let wordOnDisplay = false;
     let currentWords = lessonWords.slice(0, 7);
     let currentIndex = 0;
     let setNum = 1;
     let setSize = 7;
-    init()
+    let dieRoll = 0;
+    let tapCount = 0;
+    //let timer;
+    let translateButtonTapped = false;
+    
+    const exclamatories = ["Oops", "I Forgor", "RIP Memory", "Uh-Oh"];
+    let currentEx = 0; //Exclamatory at this index will be displayed next.
+    init();
     
     function init() {
         // Display the first word and definition
@@ -117,37 +124,54 @@ const lessonWords = [
         definition.innerHTML = currentWords[0].definition;
         displaySetNum();
         // Event listeners for the buttons
-        document.getElementById("left-button").addEventListener("click", function() {
-            this.blur();
-
-            if(wordOnDisplay === true) {
-                // Move the current word back in the queue by 3 positions
-            // or to the back if there are less than 4 words in the queue
-            let currentWord = currentWords.shift();
-            if (currentWords.length < 4) {
-                currentWords.push(currentWord);
-                console.log(currentWords)
+        document.getElementById("left-button").addEventListener("touchstart", function () {
+            if (translateButtonTapped === false) {
+                translateButtonTapped = true;
             } else {
-                currentWords.splice(3, 0, currentWord);
-                console.log(currentWords)
+                translateButtonTapped = false;
             }
-            currentWord.score = -1;
-            definition.style.visibility = "hidden";
-            definition.style.textTransform = "lowercase"
+        });
+        document.getElementById("left-button").addEventListener("click", function () {
+            this.blur();
     
-            displayDone();
-            displayNextWord();
-            document.getElementById("left-button").innerHTML = "Translate";
-            wordOnDisplay = false;
-            }
-            else {
+            if (wordOnDisplay === true) {
+                // Move the current word back in the queue by 3 positions
+                // or to the back if there are less than 4 words in the queue
+                let currentWord = currentWords.shift();
+                if (currentWords.length < 4) {
+                    currentWords.push(currentWord); //put it at the very end
+                    console.log(currentWords);
+                } else {
+                    currentWords.splice(3, 0, currentWord);
+                    console.log(currentWords);
+                }
+                currentWord.score = -1;
+                definition.style.visibility = "hidden";
+                definition.style.textTransform = "lowercase";
+    
+                displayDone();
+                displayNextWord();
+                document.getElementById("left-button").innerHTML = "Translate";
+                wordOnDisplay = false;
+            } else {
                 wordOnDisplay = true;
                 definition.style.visibility = "visible";
-                document.getElementById("left-button").innerHTML = "Again";
+                dieRoll = getRandomNumber();
+                //If dieRoll < 98, display "Again" instead of an exclamatory remark
+    
+                if (dieRoll < 98) {
+                    document.getElementById("left-button").innerHTML = "Again";
+                } else {
+                    if (currentEx > 3) {
+                        currentEx = 0;
+                    }
+                    document.getElementById("left-button").innerHTML = exclamatories[currentEx];
+                    currentEx += 1;
+                }
             }
         });
     
-        document.getElementById("right-button").addEventListener("click", function() {
+        document.getElementById("right-button").addEventListener("click", function () {
             this.blur();
             // Move the current word to the back of the queue
             let currentWord = currentWords.shift();
@@ -155,28 +179,28 @@ const lessonWords = [
             currentWord.score++;
             // If the word's score is 2, remove it from the queue
             if (currentWord.score === 2) {
-                currentWords = currentWords.filter(word => word !== currentWord);
+                currentWords = currentWords.filter((word) => word !== currentWord);
             }
             definition.style.visibility = "hidden";
-            definition.style.textTransform = "lowercase"
+            definition.style.textTransform = "lowercase";
             document.getElementById("left-button").innerHTML = "Translate";
             displayDone();
             displayNextWord();
             wordOnDisplay = false;
         });
     
-        document.getElementById("next-button").addEventListener("click", function() {
+        document.getElementById("next-button").addEventListener("click", function () {
             this.blur();
             // Load the next setSize words from the lessonWords array
             currentIndex += setSize;
             currentWords = lessonWords.slice(currentIndex, currentIndex + setSize);
             definition.style.visibility = "hidden";
-            definition.style.textTransform = "lowercase"
+            definition.style.textTransform = "lowercase";
     
             setNum += 1;
-            
+    
             displaySetNum();
-
+    
             displayDone();
             displayNextWord();
             wordOnDisplay = false;
@@ -184,100 +208,132 @@ const lessonWords = [
     
         document.addEventListener("keydown", (event) => {
             // Check if the key that was pressed is the space key
-            if (event.key === " "){
+            if (event.key === " ") {
                 const numInput = document.getElementById("set-num");
-                numInput.value = (numInput.value).replaceAll(" ", "");
-                if(wordOnDisplay === false){
+                numInput.value = numInput.value.replaceAll(" ", "");
+                if (wordOnDisplay === false) {
                     displayDone();
                     definition.style.visibility = "visible";
                     wordOnDisplay = true;
-                }
-                else {
+                    dieRoll = getRandomNumber();
+                    //If dieRoll < 98, display "Again" instead of an exclamatory remark
+                    if (dieRoll < 98) {
+                        document.getElementById("left-button").innerHTML = "Again";
+                    } else {
+                        if (currentEx > 3) {
+                            currentEx = 0;
+                        }
+                        document.getElementById("left-button").innerHTML = exclamatories[currentEx];
+                        currentEx += 1;
+                    }
+                } else {
                     let currentWord = currentWords.shift();
                     currentWords.push(currentWord);
                     currentWord.score++;
                     // If the word's score is 2, remove it from the queue
                     if (currentWord.score === 2) {
-                        currentWords = currentWords.filter(word => word !== currentWord);
+                        currentWords = currentWords.filter((word) => word !== currentWord);
                     }
                     definition.style.visibility = "hidden";
-                    definition.style.textTransform = "lowercase"
-        
+                    definition.style.textTransform = "lowercase";
+                    document.getElementById("left-button").innerHTML = "Translate";
                     displayDone();
                     displayNextWord();
                     wordOnDisplay = false;
                 }
             }
-            if (event.key === "Enter"){
+            if (event.key === "Enter") {
                 const numInput = document.getElementById("set-num");
                 numInput.blur();
                 displaySet();
             }
-            
-        })
+        });
         document.addEventListener("keydown", (event) => {
             // Check if the key that was pressed is the space key
-            if (event.key !== "1") return
-            
+            if (event.key !== "1") return;
+    
             let currentWord = currentWords.shift();
             if (currentWords.length < 4) {
                 currentWords.push(currentWord);
-                console.log(currentWords)
+                console.log(currentWords);
             } else {
                 currentWords.splice(3, 0, currentWord);
-                console.log(currentWords)
+                console.log(currentWords);
             }
             currentWord.score = -1;
             definition.style.visibility = "hidden";
-            definition.style.textTransform = "lowercase"
+            definition.style.textTransform = "lowercase";
     
             displayDone();
             displayNextWord();
             wordOnDisplay = false;
-            
-            
-        })
+        });
+    
         document.addEventListener("touchend", (event) => {
             displayDone();
-            definition.style.visibility = "visible";
-        })
-
+            if (translateButtonTapped === true) {
+                translateButtonTapped = false;
+                return;
+            }
+            if (wordOnDisplay === false) {
+                displayDone();
+                definition.style.visibility = "visible";
+                wordOnDisplay = true;
+                dieRoll = getRandomNumber();
+                //If dieRoll < 98, display "Again" instead of an exclamatory remark
+                if (dieRoll < 98) {
+                    document.getElementById("left-button").innerHTML = "Again";
+                } else {
+                    if (currentEx > 3) {
+                        currentEx = 0;
+                    }
+                    document.getElementById("left-button").innerHTML = exclamatories[currentEx];
+                    currentEx += 1;
+                }
+            }
+        });
+    
+        document.addEventListener("touchstart", (event) => {
+            tapCount++; // increment the tapCount variable when a tap is detected
+        });
     }
-    function displaySet(){
+    function displaySet() {
         const numInput = document.getElementById("set-num");
         setNum = parseInt(numInput.value);
         currentIndex = setNum * setSize - setSize;
-
+    
         currentWords = lessonWords.slice(currentIndex, currentIndex + setSize);
         definition.style.visibility = "hidden";
         definition.style.textTransform = "lowercase";
-
+    
         displaySetNum();
-
+    
         displayDone();
         displayNextWord();
-        
     }
-
+    
     function displaySetNum() {
         //Find input box
         const numInput = document.getElementById("set-num");
         numInput.value = setNum;
     }
     
-    
     function displayNextWord() {
-      // Display the next word and definition
-      let currentWord = currentWords[0];
-      document.getElementById("word").innerHTML = currentWord.word;
-      definition.innerHTML = currentWord.definition;
+        // Display the next word and definition
+        let currentWord = currentWords[0];
+        document.getElementById("word").innerHTML = currentWord.word;
+        definition.innerHTML = currentWord.definition;
     }
-    
     
     function displayDone() {
-            if (currentWords.length !== 0) return
-            const definition =  document.getElementById("definition")
-            definition.style.visibility = "visible";
-            definition.innerHTML = "Set Done " + setNum
-            definition.style.textTransform = "capitalize"
+        if (currentWords.length !== 0) return;
+        const definition = document.getElementById("definition");
+        definition.style.visibility = "visible";
+        definition.innerHTML = "Set Done " + setNum;
+        definition.style.textTransform = "capitalize";
     }
+    
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 100) + 1;
+    }
+    
