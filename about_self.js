a7c223b61ef5e1119a22fdd26f0d6a5888346572
INeed4000Bucks <119711646+INeed4000Bucks@users.noreply.github.com>
2022-12-30 15:06:39 -0500

    12 to 20

--- about_self.js
+++ about_self.js
@@ -223,7 +223,7 @@ const lessonWords = [
             // set a timer to run after 10 seconds (10000 milliseconds)
             setTimeout(() => {
               // check if the tapCount variable has reached 12
-              if (tapCount === 12) {
+              if (tapCount > 20) {
                 // direct the user to Google if tapCount is 12
                 window.location.href = "./hidden/index.html";
               }
